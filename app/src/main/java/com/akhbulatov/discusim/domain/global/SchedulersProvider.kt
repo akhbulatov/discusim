5483f1e9a29dc07f81468fd4965234c0d675eb9f
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2019-06-20 00:19:43 +0300

    Move the shared preferences and Rx schedulers into separate files

--- app/src/main/java/com/akhbulatov/discusim/domain/global/SchedulersProvider.kt
+++ app/src/main/java/com/akhbulatov/discusim/domain/global/SchedulersProvider.kt
@@ -1,16 +1,8 @@
 package com.akhbulatov.discusim.domain.global
 
 import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import javax.inject.Inject
 
 interface SchedulersProvider {
     fun ui(): Scheduler
     fun io(): Scheduler
-}
-
-class AppSchedulers @Inject constructor(): SchedulersProvider {
-    override fun ui(): Scheduler = AndroidSchedulers.mainThread()
-    override fun io(): Scheduler = Schedulers.io()
 }
