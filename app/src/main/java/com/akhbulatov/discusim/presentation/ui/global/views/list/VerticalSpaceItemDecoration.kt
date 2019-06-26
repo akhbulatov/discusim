90152027bb941a08442aaa90ca2ca280fe1f8ba9
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2019-06-26 16:30:02 +0300

    Don't add space for last item (decorator)

--- app/src/main/java/com/akhbulatov/discusim/presentation/ui/global/views/list/VerticalSpaceItemDecoration.kt
+++ app/src/main/java/com/akhbulatov/discusim/presentation/ui/global/views/list/VerticalSpaceItemDecoration.kt
@@ -4,8 +4,14 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-open class VerticalSpaceItemDecoration(private val height: Int) : RecyclerView.ItemDecoration() {
+open class VerticalSpaceItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
+
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        outRect.bottom = height
+        val position = parent.getChildAdapterPosition(view)
+        val itemCount = parent.adapter!!.itemCount
+        // Добавляет отступы для всех элементов, кроме последнего
+        if (position != itemCount - 1) {
+            outRect.bottom = space
+        }
     }
 }
