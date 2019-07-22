b8208e1c94b0770fe9879a6264d1e67fffe5efe6
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2019-07-22 15:17:13 +0300

    Make channel's avatar url nullable

--- app/src/main/java/com/akhbulatov/discusim/data/global/network/models/forum/ChannelNetModel.kt
+++ app/src/main/java/com/akhbulatov/discusim/data/global/network/models/forum/ChannelNetModel.kt
@@ -7,7 +7,7 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class ChannelNetModel(
     @Json(name = "id") val id: String,
-    @Json(name = "avatar") val avatar: String,
+    @Json(name = "avatar") val avatar: String?,
     @Json(name = "banner") val banner: String?,
     @Json(name = "bannerColorHex") @HexColor val bannerColorHex: Int,
     @Json(name = "options") val options: OptionsNetModel
--- app/src/main/java/com/akhbulatov/discusim/domain/global/models/forum/Channel.kt
+++ app/src/main/java/com/akhbulatov/discusim/domain/global/models/forum/Channel.kt
@@ -2,7 +2,7 @@ package com.akhbulatov.discusim.domain.global.models.forum
 
 data class Channel(
     val id: Long,
-    val avatarUrl: String,
+    val avatarUrl: String?,
     val bannerUrl: String?,
     /** Используется в случае, когда [bannerUrl] равен `null`. **/
     val bannerColorHex: Int
